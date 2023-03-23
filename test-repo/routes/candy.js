6a0c8d07e43df2f73388068158921ada55d60397
pfulcher26 <pfulcher26@gmail.com>
2023-03-23 10:32:44 -0700

    Adds an endpoint for /candies that returns the candies array as a json object in its response.

--- test-repo/routes/candy.js
+++ test-repo/routes/candy.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const candies = require('../models/candy');
+
+//GET  /candies
+router.get('/', (req ,res) => {
+    res.json(candies);
 });
 
 module.exports = router;
