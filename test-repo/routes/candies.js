4eefb8464ea0b833256b7a7f9b995813f6ec96f5
pfulcher26 <pfulcher26@gmail.com>
2023-03-23 14:29:17 -0700

    Fixes routes.

--- test-repo/routes/candies.js
+++ test-repo/routes/candies.js
@@ -10,22 +10,22 @@ router.get('/', (req ,res) => {
 
 //POST /candies
 router.post('/', (req, res) => {
-  candies.push(req.body);
-  // sends back a response status that the resource has been created
-  // and the candy in json format 
-  res.status(201).json(req.body)
+    candies.push(req.body);
+    // sends back a response status that the resource has been created
+    // and the candy added in json format 
+    res.status(201).json(candies)
 });
 
 // DELETE /candies/:id
 router.delete('/:id', (req, res) => {
-  const id = req.params.id;
-  const index = candies.findIndex(candy => candy.id == id);
-  if (index !== -1) {
-  candies.splice(index, 1);
-  res.status(204).json({ message: 'Candy deleted successfully.' });
-  } else {
-  res.status(404).json({ message: `Candy with ID ${id} not found.` });
-  }
+    const id = req.params.id;
+    const index = candies.findIndex(candy => candy.id == id);
+    if (index !== -1) {
+    candies.splice(index, 1);
+    res.status(204).json({ message: 'Candy deleted successfully.' });
+    } else {
+    res.status(404).json({ message: `Candy with ID ${id} not found.` });
+    }
 });
 
 module.exports = router;
