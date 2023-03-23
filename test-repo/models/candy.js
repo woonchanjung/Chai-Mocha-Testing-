be4c815606570eef28ed38f6453615d3022cd4ff
pfulcher26 <pfulcher26@gmail.com>
2023-03-23 09:52:00 -0700

    Creates a models directory.  Creates a candy.js file within the models directory and populates it with mock data.

---
+++ test-repo/models/candy.js
@@ -0,0 +1,21 @@
+const candies = [
+    {
+        id: 1,
+        name: 'Toffee Bar',
+        color: 'Brown, Caramel'
+    }, {
+        id: 2,
+        name: 'Pez',
+        color: 'Green'
+    }, {
+        id: 3,
+        name: 'Pop Rocks',
+        color: 'Pink'
+    }, {
+        id: 4,
+        name: 'Sour Patch Kids',
+        color: 'Blue'
+    }
+];
+
+module.exports = candies; 
